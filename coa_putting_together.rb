72c00764f509e29690325534e3ddadf5fbefae8e
Alice Pike <alicepike123@hotmail.com>
2013-04-06 13:25:32 +0100

    q6 buggy

--- coa_putting_together.rb
+++ coa_putting_together.rb
@@ -81,6 +81,10 @@ end
 
 puts "\n\nProblem 6:"
 
+(responses[:neutral]).each do 
+	print "responses[:neutral]"
+end
+
 # 7. You can also loop through Hashes. Loop through our `responses`
 #    hash, printing each Array to the screen.
 
